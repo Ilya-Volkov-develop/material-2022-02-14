4e170c49102243d7b5a4121337166197b090789d
Илья <ilia.volkov2016@mail.ru>
2022-02-16 00:23:27 +0300

    Урок 1. Обработка ошибки с сервера

--- app/src/main/java/ru/iliavolkov/material/view/mainfragment/MainFragment.kt
+++ app/src/main/java/ru/iliavolkov/material/view/mainfragment/MainFragment.kt
@@ -1,12 +1,17 @@
 package ru.iliavolkov.material.view.mainfragment
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import coil.load
+import ru.iliavolkov.material.R
 import ru.iliavolkov.material.databinding.FragmentMainBinding
 import viewmodel.PictureOfTheDayViewModel
 import viewmodel.appstate.AppStatePictureOfTheDay
@@ -31,7 +36,9 @@ class MainFragment : Fragment() {
 
     private fun renderData(it: AppStatePictureOfTheDay?) {
         when(it){
-            is AppStatePictureOfTheDay.Error -> TODO()
+            is AppStatePictureOfTheDay.Error -> {
+                loadingFailed(it.error,it.code)
+            }
             is AppStatePictureOfTheDay.Loading -> {
                 binding.loadingLayout.visibility = View.VISIBLE
             }
@@ -43,8 +50,30 @@ class MainFragment : Fragment() {
         }
     }
 
-    companion object {
+    //при ошибке всплывает диалоговое окно
+    private fun loadingFailed(textId: Int, code: Int) {
+        val dialog: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        val inflater: LayoutInflater? = LayoutInflater.from(requireContext())
+        val exitView: View = inflater!!.inflate(R.layout.dialog_error, null)
+        dialog.setView(exitView)
+        val dialog1: Dialog? = dialog.create()
+        val ok: Button = exitView.findViewById(R.id.ok)
+        val codeTextView = exitView.findViewById<TextView>(R.id.textError)
 
+        codeTextView.text = when(textId) {
+            R.string.errorOnServer -> getString(R.string.errorOnServer)
+            R.string.codeError -> getString(R.string.codeError) + " " + code
+            else -> ""
+        }
+        dialog1?.setCancelable(false)
+        ok.setOnClickListener {
+            dialog1?.dismiss()
+            requireActivity().onBackPressed()
+        }
+        dialog1?.show()
+    }
+
+    companion object {
         @JvmStatic
         fun newInstance() = MainFragment()
     }
