50392a4d3034f3e19aec4b2e31a14c9742cf584b
Илья <ilia.volkov2016@mail.ru>
2022-02-25 14:06:40 +0300

    Урок 3. BottomNavigationView

--- app/src/main/java/ru/iliavolkov/material/view/main/MainFragment.kt
+++ app/src/main/java/ru/iliavolkov/material/view/main/MainFragment.kt
@@ -5,24 +5,19 @@ import android.app.Dialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import coil.load
-import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.tabs.TabLayout
 import ru.iliavolkov.material.R
 import ru.iliavolkov.material.databinding.FragmentMainBinding
-import ru.iliavolkov.material.view.MainActivity
-import ru.iliavolkov.material.view.setting.SettingFragment
-import ru.iliavolkov.material.view.navigationDrawerFragment.BottomNavigationDrawerFragment
 import ru.iliavolkov.material.viewmodel.PictureOfTheDayViewModel
 import ru.iliavolkov.material.viewmodel.appstate.AppStatePictureOfTheDay
 import java.text.SimpleDateFormat
@@ -48,25 +43,18 @@ class MainFragment : Fragment() {
         viewModel.getLiveData().observe(viewLifecycleOwner, { renderData(it) })
         viewModel.getPictureOfTheDay(takeDate(0))
         clickInputLayout()
-        clickFAB()
         tabLayoutInit()
+        bottomSheet()
+    }
 
+    private fun bottomSheet() {
         behavior.addBottomSheetCallback(object :
-            BottomSheetBehavior.BottomSheetCallback() {
+                BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {}
 
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                if (slideOffset == 0.0f) {
-                    binding.fab.visibility = View.VISIBLE
-                    binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
-                    binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
-                }
             }
         })
-
-
-        (requireActivity() as MainActivity).setSupportActionBar(binding.bottomAppBar)
-        setHasOptionsMenu(true)
     }
 
     private fun tabLayoutInit() {
@@ -98,32 +86,8 @@ class MainFragment : Fragment() {
             })
         }
     }
-    private fun clickFAB() {
-        binding.fab.setOnClickListener{
-            behavior.state = BottomSheetBehavior.STATE_EXPANDED
-            binding.fab.visibility = View.INVISIBLE
-        }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.menu_bottom_bar, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.app_bar_fav -> {
-                Toast.makeText(requireContext(), "app_bar_fav", Toast.LENGTH_SHORT).show()
-            }
-            R.id.app_bar_settings -> {
-                requireActivity().supportFragmentManager.beginTransaction().replace(R.id.container, SettingFragment.newInstance(),"ChipsFragment").addToBackStack("").commit()
-            }
-            android.R.id.home -> {
-                BottomNavigationDrawerFragment().show(requireActivity().supportFragmentManager,"BottomNavigationDrawerFragment")
-            }
-        }
-        return super.onOptionsItemSelected(item)
-    }
+    //requireActivity().supportFragmentManager.beginTransaction().replace(R.id.container, SettingFragment.newInstance(),"ChipsFragment").addToBackStack("").commit()
 
     private fun renderData(it: AppStatePictureOfTheDay?) {
         when(it){
@@ -160,7 +124,6 @@ class MainFragment : Fragment() {
         dialog1?.setCancelable(false)
         ok.setOnClickListener {
             dialog1?.dismiss()
-            requireActivity().onBackPressed()
         }
         dialog1?.show()
     }
